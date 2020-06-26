3d326aab39f379650b1c5e626ce6cee66e1f5c2c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-06-26 17:42:46 -0500

    drop dead code

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
         exec: {
             test: 'npm test',
             cucumber: 'bundle exec cucumber',
-            jsfmt: 'find . -type d -name node_modules -prune -o -type f -name "*[.-]min.js" -prune -o \\( -type f -name "*.js" -o -type f -path "*/bin/*" \\) -exec node node_modules/.bin/jsfmt -w {} \\;',
             jshint: 'node node_modules/jshint/bin/jshint .',
             jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;'
         }
@@ -18,13 +17,11 @@ module.exports = function(grunt) {
     grunt.registerTask('cucumber', ['exec:cucumber']);
 
     grunt.registerTask('lint', [
-        'exec:jsfmt',
         'exec:jshint',
         // Go to bed, Crockford
         // 'exec:jslint'
     ]);
 
-    grunt.registerTask('jsfmt', ['exec:jsfmt']);
     grunt.registerTask('jshint', ['exec:jshint']);
     grunt.registerTask('jslint', ['exec:jslint']);
 };
