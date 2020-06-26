88c029e76fc7c2d5a9330707acc24db9aec7673b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-06-26 17:28:31 -0500

    drop dead code

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,7 @@ module.exports = function(grunt) {
             jsfmt: 'find . -type d -name node_modules -prune -o -type f -name "*[.-]min.js" -prune -o \\( -type f -name "*.js" -o -type f -path "*/bin/*" \\) -exec node node_modules/.bin/jsfmt -w {} \\;',
             jshint: 'node node_modules/jshint/bin/jshint .',
             jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
-            eslint: 'node node_modules/eslint/bin/eslint .',
-            editorconfig: 'git ls-files -z | grep -av patch | xargs -0 -r -n 100 node_modules/.bin/eclint check'
+            eslint: 'node node_modules/eslint/bin/eslint .'
         }
     });
 
@@ -24,13 +23,11 @@ module.exports = function(grunt) {
         'exec:jshint',
         // Go to bed, Crockford
         // 'exec:jslint',
-        'exec:eslint',
-        'exec:editorconfig'
+        'exec:eslint'
     ]);
 
     grunt.registerTask('jsfmt', ['exec:jsfmt']);
     grunt.registerTask('jshint', ['exec:jshint']);
     grunt.registerTask('jslint', ['exec:jslint']);
     grunt.registerTask('eslint', ['exec:eslint']);
-    grunt.registerTask('editorconfig', ['exec:editorconfig']);
 };
