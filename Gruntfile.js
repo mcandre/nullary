667cd5f7c8cfc4c7eaa1e67cb887be3ee7455374
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-06-26 17:40:06 -0500

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,7 @@ module.exports = function(grunt) {
             cucumber: 'bundle exec cucumber',
             jsfmt: 'find . -type d -name node_modules -prune -o -type f -name "*[.-]min.js" -prune -o \\( -type f -name "*.js" -o -type f -path "*/bin/*" \\) -exec node node_modules/.bin/jsfmt -w {} \\;',
             jshint: 'node node_modules/jshint/bin/jshint .',
-            jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
-            eslint: 'node node_modules/eslint/bin/eslint .'
+            jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;'
         }
     });
 
@@ -22,12 +21,10 @@ module.exports = function(grunt) {
         'exec:jsfmt',
         'exec:jshint',
         // Go to bed, Crockford
-        // 'exec:jslint',
-        'exec:eslint'
+        // 'exec:jslint'
     ]);
 
     grunt.registerTask('jsfmt', ['exec:jsfmt']);
     grunt.registerTask('jshint', ['exec:jshint']);
     grunt.registerTask('jslint', ['exec:jslint']);
-    grunt.registerTask('eslint', ['exec:eslint']);
 };
